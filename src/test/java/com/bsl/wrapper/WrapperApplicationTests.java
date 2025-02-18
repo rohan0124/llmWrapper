33c5f0d2f4cb540f2fbd7de4c8b98116bb4e04ca
Rohan Thakur <64641108+rohan0124@users.noreply.github.com>
2025-02-18 22:58:15 +0530

    First Commit
    Setting up spring web app

---
+++ src/test/java/com/bsl/wrapper/WrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsl.wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
